ef9928f551dda2134ce7e9b2494b9c6df84703b7
pipetky <odmin@p-c.ru>
2018-12-13 13:55:14 +0300

    with descriptor

--- main.c
+++ main.c
@@ -3,24 +3,30 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/resource.h>
-
+#include <fcntl.h>
+#define BUFFER_SIZE	64
 
 #define ERROR_CREATE_THREAD -11
 #define ERROR_JOIN_THREAD   -12
 #define SUCCESS        0
 
-void * file_read_th(FILE * file) {
-    char str[1024];
-    while (fscanf(file, "%s", str) != EOF)
+void * file_read_th(int *fd) {
+
+    ssize_t read_bytes;
+    char buffer[BUFFER_SIZE+1];
+    while ((read_bytes = read (*fd, buffer, BUFFER_SIZE)) > 0)
     {
-        printf("%s\n",str);
-
+        buffer[read_bytes] = 0; /* Null-terminator for C-string */
+        fputs (buffer, stdout);
     }
-    fclose(file);
 
-
-
-    printf("Hello from thread!\n");
+    if (read_bytes < 0)
+    {
+        fprintf (stderr, "myread: Cannot read file\n");
+        exit (1);
+    }
+    close (*fd);
+    printf("\nHello from thread!\n");
 
     return SUCCESS;
 }
@@ -29,12 +35,20 @@ void * file_read_th(FILE * file) {
 
 
 int main() {
-    FILE *f = fopen("file.txt", "r");
+    int fd = 0;
+
+    fd = open ("file.txt", O_RDONLY);
+    if (fd < 0)
+    {
+        fprintf (stderr, "openclose: Cannot open file '%s'\n",
+                "file.txt");
+        exit (1);
+    }
+
     pthread_t thread;
     int status;
 
-
-    status = pthread_create(&thread, NULL, file_read_th, f);
+    status = pthread_create(&thread, NULL, file_read_th, &fd);
     if (status != SUCCESS) {
         printf("main error: can't create thread, status = %d\n", status);
         exit(ERROR_CREATE_THREAD);
